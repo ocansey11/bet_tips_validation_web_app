c6d6e323f784bbc7b67de89b1578922da3eb4cb8
kevin ocansey <kevinocansey11@gmail.com>
2024-08-14 09:39:51 +0000

    I created routes for matches (completed & upcoming) to handle scrapping completed and upcoming matches from forebet.com. I did the same for league standings (current and previous)

--- backend/app/routes/__init__.py
+++ backend/app/routes/__init__.py
@@ -1,1 +1,3 @@
 from .admin import admin_bp
+from .league_standings import standings_bp
+from .matches import matches_bp
---
+++ backend/app/routes/matches.py
@@ -0,0 +1,23 @@
+# routes/complete_matches.py
+from flask import Blueprint, jsonify # type: ignore
+from app.scraping import cm_scrapper, um_scrapper, url_forebet, forebet_className,user,password,host,port,dbname
+
+matches_bp = Blueprint('matches', __name__)
+
+@matches_bp.route('/completed', methods=['GET'])
+def completed():
+    try:
+        cm_scrapper(url_forebet,forebet_className,user,password,host,port,dbname)
+        return jsonify({'message': 'Scraping completed and data stored successfully'}), 200
+    except Exception as e:
+        return jsonify({'error': str(e)}), 500
+
+@matches_bp.route('/upcoming', methods=['GET'])
+def upcoming():
+    try:
+        um_scrapper(url_forebet,forebet_className,user,password,host,port,dbname)
+        return jsonify({'message': 'Scraping completed and data stored successfully'}), 200
+    except Exception as e:
+        return jsonify({'error': str(e)}), 500
+    
+    url,className,user,password,host,port,dbname
