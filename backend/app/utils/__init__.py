ddec91e8e863a6ddf748ff22355fcc72747b336a
kevin ocansey <kevinocansey11@gmail.com>
2024-08-18 21:53:59 +0000

    changed the URI to make it more modular ?

--- backend/app/utils/__init__.py
+++ backend/app/utils/__init__.py
@@ -1,3 +1,2 @@
 # app/utils/__init__.py
 from .db_utils import db, migrate
-# from .sql_utils import CREATE_COMPLETED_MATCHES_TABLE, CREATE_UPCOMING_MATCHES_TABLE, CREATE_PREVIOUS_WEEK_STANDINGS_TABLE
