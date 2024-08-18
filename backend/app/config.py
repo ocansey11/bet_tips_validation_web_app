39f3133d6a77480ce7134413d81aca64e451e0fb
kevin ocansey <kevinocansey11@gmail.com>
2024-08-18 21:57:52 +0000

    little tweaks to URI

--- backend/app/config.py
+++ backend/app/config.py
@@ -1,11 +1,13 @@
 import os
+from app.scraping import user,password,host,dbname,port
 
 class Config:
-    SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://test_user:password@localhost/bet_model_testing'
+    SQLALCHEMY_DATABASE_URI = f'mysql+pymysql://{user}:{password}@{host}/{dbname}'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
 
 class DevelopmentConfig(Config):
     SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://user:password@localhost/dev_db'
 
 class TestingConfig(Config):
-    SQLALCHEMY_DATABASE_URI = 'sqlite:///:memory:'
+    SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://test_user:password@localhost/test_db'
+    # SQLALCHEMY_DATABASE_URI = 'sqlite:///:memory:'
